6bdf9be4d6d88d09c3219fa0ca10bbd4db2e434b
meteorlxy <meteor.lxy@foxmail.com>
2023-02-21 10:03:39 +0800

    test: update test fixtures

---
+++ tests/parse-iwa.spec.ts
@@ -0,0 +1,20 @@
+import path from 'node:path';
+import fastGlob from 'fast-glob';
+import { describe, expect, it } from 'vitest';
+import { parseIwa } from '../src';
+
+const iwaDirectory = path.resolve(__dirname, '../__fixtures__/iwa');
+
+describe('parseIwa', () => {
+  it('should work well', async () => {
+    const iwaFilePaths = await fastGlob('**/*.iwa', {
+      cwd: iwaDirectory,
+      absolute: true,
+    });
+
+    for (const iwaFilePath of iwaFilePaths) {
+      const iwaData = await parseIwa(iwaFilePath);
+      expect(iwaData).toMatchSnapshot();
+    }
+  });
+});
