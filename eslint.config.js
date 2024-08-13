2d6e516894be0b5192fd5130ae2f74b4ceb7d1ff
meteorlxy <meteor.lxy@foxmail.com>
2024-08-13 20:36:17 +0800

    build: bump deps

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,6 @@ const { meteorlxy } = require('@meteorlxy/eslint-config');
 module.exports = meteorlxy({
   ignores: ['lib/', 'proto/'],
   typescript: {
-    tsconfigPath: './tsconfig.json',
     overrides: {
       'no-console': 'off',
     },
